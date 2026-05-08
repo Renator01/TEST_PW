33339c30675d719a0e81ba90c6b597600c57246a
Renato <renato.rodrigues@gvclinicas.com.br>
2026-05-08 12:07:28 -0300

    tudo correto

--- tests/Testes01/DateTest.spec.ts
+++ tests/Testes01/DateTest.spec.ts
@@ -6,6 +6,30 @@ test('Assert in playwright', async ({page}) => {
     // Assert url 
     await expect(page).toHaveURL('https://www.w3schools.com/tags/tryit.asp?filename=tryhtml_select');
     await page.waitForTimeout(2000);
+    await page.frameLocator('#iframeResult').locator('select').selectOption('saab');
+    // Assert value of select option
+    const selectedOption = await page.frameLocator('#iframeResult').locator('select').inputValue();
+    expect(selectedOption).toBe('saab');
+    
+    // Assert all options in select dropdown
+    const options = await page.frameLocator('#iframeResult').locator('select option').allTextContents();
+    expect(options).toEqual(['Volvo', 'Saab', 'Opel', 'Audi']);
+
+    // Assert that the select element is visible
+    await expect(page.frameLocator('#iframeResult').locator('select')).toBeVisible();
+    
+ 
+    // Assert that the select element is enabled
+    await expect(page.frameLocator('#iframeResult').locator('select')).toBeEnabled();
+
+    // Assert that the select element has the correct number of options
+    const optionCount = await page.frameLocator('#iframeResult').locator('select option').count();
+    expect(optionCount).toBe(4);
+
+ 
+
+
+    
 
    
   
--- tests/Testes01/RecordTest.spec.ts
+++ tests/Testes01/RecordTest.spec.ts
@@ -3,7 +3,7 @@ import { test, expect } from "@playwright/test";
 test.beforeEach('Run before each test @Smoke', async ({ page }) => {
   console.log('Running before each test');
   await page.goto("https://www.youtube.com/");
-  await page.waitForTimeout(6000);
+  await page.waitForTimeout(2000);
 });
 test('test click @Smoke', async ({ page }) => {
   
@@ -11,9 +11,7 @@ test('test click @Smoke', async ({ page }) => {
   await page.getByPlaceholder('Search').fill('playwright');
   await page.getByRole('button', { name: 'Search', exact: true }).click();
   await page.waitForTimeout(1000);
-  await page.getByRole('link', { name: 'Playwright Tutorial for Beginners | Playwright Full Course | Edureka' }).click();
-  await page.waitForTimeout(1000);
-  await expect(page.getByRole('heading', { name: 'Playwright Tutorial for Beginners | Playwright Full Course | Edureka' })).toBeVisible();
+ 
 });
 
 //npx playwright test codegen ele fica gravando seu passo a passo e gera o codigo ao lado para vc copiar
