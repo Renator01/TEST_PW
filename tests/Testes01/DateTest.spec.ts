bfa7745616f45e2ea3ae46c4b01b9b06cc052a2c
Renato <renato.rodrigues@gvclinicas.com.br>
2026-05-11 14:33:33 -0300

    ..

--- tests/Testes01/DateTest.spec.ts
+++ tests/Testes01/DateTest.spec.ts
@@ -1,13 +1,29 @@
 import {test, expect} from "@playwright/test";
 
-test('Assert in playwright', async ({page}) => {
-    await page.goto('https://www.w3schools.com/tags/tryit.asp?filename=tryhtml_select')
-    
-    // Assert url 
-    await expect(page).toHaveURL('https://www.w3schools.com/tags/tryit.asp?filename=tryhtml_select');
-    await page.waitForTimeout(1000);
-
-   
+test('Entrando em JS Introduction', async ({page}) => {
+   await page.goto('https://www.w3schools.com/');
+   await page.getByRole('link', { name: 'JavaScript Tutorial' }).click();
+   await page.waitForTimeout(1000);
+   await page.getByRole('link', { name: 'JS Introduction' }).click();
+   await page.waitForTimeout(1000);
+   await expect(page.getByRole('heading', { name: 'JavaScript Introduction' })).toBeVisible();
   
+});
 
+test('Entrando em JS Functions', async ({page}) => {
+  await page.goto('https://www.w3schools.com/');
+  await page.getByRole('link', { name: 'JavaScript Tutorial' }).click();
+  await page.locator('a.overview_header[href="js_functions.asp"]').click();
+  await page.waitForTimeout(1000);
+  await expect(page.getByRole('heading', { name: 'JavaScript Functions' })).toBeVisible();
+});
+
+test('Entrando em JS Objects', async ({page}) => {
+  await page.goto('https://www.w3schools.com/');
+  await page.getByRole('link', { name: 'JavaScript Tutorial' }).click();
+  await page.locator('a.overview_header[href="js_objects.asp"]').click();
+  await page.waitForTimeout(1000);
+  await expect(page.getByRole('heading', { name: 'JavaScript Objects', level: 1
+    
+   })).toBeVisible();
 });
