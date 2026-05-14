e6f736b5d9d6fa244c06c7e2bcc7338c1518144c
Renator01 <renato.rodrigues@univale.br>
2026-05-14 10:07:20 -0300

    Learn more

--- tests/Testes01/DropDrownList.spec.ts
+++ tests/Testes01/DropDrownList.spec.ts
@@ -8,6 +8,9 @@ test.describe('Example.com', () => {
 
   test('should display correct page title', async ({ page }) => {
     await expect(page).toHaveTitle(/Example Domain/);
+    await page.getByRole('link', { name: 'Learn more' }).click();
+     await expect(page).toHaveURL(/iana\.org\/help\/example-domains/);
+
   });
 
 });
